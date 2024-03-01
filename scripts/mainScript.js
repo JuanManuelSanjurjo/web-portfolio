397bf215470bfbd020c2b14bff7ad5f45e83e1ff
JuanManuelSanjurjo <juanmanuelsanjurjo@hotmail.com>
2024-03-01 16:45:38 -0300

    split intersection observer for floating divs and for sections for smoother transitions

--- scripts/mainScript.js
+++ scripts/mainScript.js
@@ -6,7 +6,7 @@ const sections = document.querySelectorAll("section");
 const options = {
   root: null, // Usar el viewport como raíz
   rootMargin: "0px",
-  threshold: 0.1, // Activa el callback cuando el  10% de la sección está visible
+  threshold: 0.005, // Activa el callback cuando el  10% de la sección está visible
 };
 
 // Definir la función de callback
@@ -14,25 +14,40 @@ const onIntersect = (entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       floatingDiv.textContent = entry.target.id;
-      floatingDiv.classList.remove("notVisible");
-      floatingDiv.classList.add("visible");
+      // floatingDiv.classList.remove("notVisible");
+      // floatingDiv.classList.add("visible");
       // entry.target.classList.remove("notVisible");
       entry.target.classList.add("visible");
     } else {
-      floatingDiv.classList.remove("visible");
-      floatingDiv.classList.add("notVisible");
+      // floatingDiv.classList.remove("visible");
+      // floatingDiv.classList.add("notVisible");
       entry.target.classList.remove("visible");
       // entry.target.classList.add("notVisible");
     }
   });
 };
 
+const onIntersectFloatingDiv = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      floatingDiv.classList.remove("notVisible");
+      floatingDiv.classList.add("visible");
+      entry.target.classList.add("visible");
+    } else {
+      floatingDiv.classList.remove("visible");
+      floatingDiv.classList.add("notVisible");
+    }
+  });
+};
+
 // Crear el observador
 const observer = new IntersectionObserver(onIntersect, options);
+const observerFloatingDiv = new IntersectionObserver(onIntersectFloatingDiv, {threshold: 0.2})
 
 // Observar cada sección
 sections.forEach((section) => {
   observer.observe(section);
+  observerFloatingDiv.observe(section);
 });
 
 const emailInput = document.querySelector("#email");
